6fa520b8432f6a160b9723971c38aebb3b5e41ae
hidayatulmusrifah <hdytlmsrfh3@gmail.com>
2022-11-08 14:08:52 +0700

    first build repository

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var config = {
 	database: {
 		// host:	  'localhost', 		// database host
-		host:	  '127.0.0.1', 		// database host
+		host:	  'localhost', 		// database host
 		user: 	  'root', 			// your database username
-		password: 'root', 			// your database password
+		password: '', 			// your database password
 		port: 	  3306, 			// default MySQL port
 		db: 	  'node_countries' // your database name
 	},
 	server: {
-		host:'127.0.0.1', // the host for the server
+		host:'localhost', // the host for the server
 		port: '3000' // the port for the server
 	}
 }
